f5e237a585b8bdc56cdde11046e98813d59dbf89
sadanya <2930048068@qq.com>
2021-07-08 18:12:36 +0800

    添加新的拖拽方法和修复旧的拖拽方法 @2.2.5

--- method/dragmove.ts
+++ method/dragmove.ts
@@ -1,26 +1,31 @@
 /**
  * 实现元素随鼠标移动的功能
- * @param el HTML 元素, 指定那个元素需要移动
+ * @param el HTML 元素, 指定哪个元素需要移动
  */
 export function dragmove(el: HTMLElement) {
-  el.onmousedown = function (event) {
+  el.onmousedown = e => {
     // 计算鼠标在元素上偏移量, 鼠标在视口的位置 - 元素在视口的偏移量
-    let mouseX = event.clientX - el.offsetLeft;
-    let mouseY = event.clientY - el.offsetTop;
+    let mouseX = e.clientX - el.offsetLeft;
+    let mouseY = e.clientY - el.offsetTop;
 
     // 在 onmousedown 中触发 nonmousemove 事件
-    document.onmousemove = function (event) {
-      event.preventDefault()
+    document.onmousemove = e => {
+      e.preventDefault()
+
+      // 防止超出视口范围
+      if (e.clientX >= innerWidth) return
+      if (e.clientY >= innerHeight) return
 
       // 改变元素定位偏移量
-      el.style.left = event.clientX - mouseX + "px";
-      el.style.top = event.clientY - mouseY + "px";
-    };
-  };
+      el.style.left = e.clientX - mouseX + "px";
+      el.style.top = e.clientY - mouseY + "px";
+    }
+  }
+
   // onmouseup 给 document 元素加上比较好 ???
-  el.onmouseup = function () {
+  el.onmouseup = () => {
     // 当鼠标松开时关闭 onmousemove 事件
     document.onmousemove = null;
     // console.log("are you sure ?");
-  };
+  }
 }
--- method/dragsort.ts
+++ method/dragsort.ts
@@ -1,8 +1,69 @@
+/**
+ * 实现拖拽排序
+ * @param el 排序列表元素的父元素, 类似 ul > li 中的 ul 元素, 排序的则是 li 元素  
+ * dragstart 和 dragend 发生在拖拽源元素上, 源元素 draggable 需为 true  
+ * dragenter 发生在目标元素上, 不需要 draggable 为 true  
+ */
 export function dragsort(el: HTMLElement) {
-  let start = null
-  let timer = undefined
-  
-  el.ondragstart = e => {
-    start = e.target
+  let start: HTMLElement
+  let timer = -1
+
+  // 事件加在父元素上实现事件委托
+  el.ondragstart = e => start = e.target as HTMLElement
+
+  el.ondragenter = e => {
+    // 应保证 enter 与 start 元素层级一致
+    let enter = e.target as HTMLElement // 没有嵌套
+    // let enter = (e.target as HTMLElement).parentElement // 再嵌套一级
+
+    // 筛选不必要的触发
+    if (enter == start) return  // 自己进入自己
+    if (enter.nodeName != start.nodeName) return // 防止层级不一样
+    if (timer != -1) return // 防鬼畜
+
+    // 获取源元素与目标元素初始位置
+    let preStartY = start.offsetTop
+    let preEnterY = enter.offsetTop
+
+    // 改变两元素的顺序
+    if (_index(start) < _index(enter)) el.insertBefore(enter, start)
+    else el.insertBefore(start, enter)
+
+    // 获取改变之后的元素位置
+    let curStartY = start.offsetTop
+    let curEnterY = enter.offsetTop
+
+    // 让元素瞬间回到初始位置
+    _transform(start, '', `translateY(${preStartY - curStartY}px)`)
+    _transform(enter, '', `translateY(${preEnterY - curEnterY}px)`)
+    // 添加动画
+    setTimeout(() => {  // 触发重绘, 实现 css 的 animation 效果
+      _transform(start, 'all .2s', 'translateY(0)')
+      _transform(enter, 'all .2s', 'translateY(0)')
+    }, 0)
+
+    // 防鬼畜
+    timer = setTimeout(() => {
+      _transform(start, '', '')
+      _transform(enter, '', '')
+      timer = -1
+    }, 200)
   }
+}
+
+/**
+ * 获取同一层级的元素下标
+ */
+export function _index(el: HTMLElement) {
+  let index = 0
+  while (el.previousElementSibling && (el = el.previousElementSibling as HTMLElement)) index++
+  return index
+}
+
+/**
+ * 添加动画
+ */
+export function _transform(el: HTMLElement, transition: string, transform: string) {
+  el.style.transition = transition
+  el.style.transform = transform
 }
