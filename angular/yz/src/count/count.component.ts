432285e9445559af652da5249cc791bc60e40d04
sadanya <2930048068@qq.com>
2021-09-09 17:41:11 +0800

    完善 count 组件功能 @1.2.1

--- angular/yz/src/count/count.component.ts
+++ angular/yz/src/count/count.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core'
 
-import { debounce } from '../common/method/debounce'
+import { debounce } from '../common/method';
 
 @Component({
   selector: 'count',
@@ -15,22 +15,26 @@ export class CountComponent {
 
   @Output() valueChange = new EventEmitter<number>() // 传出改变后的值
 
+  @Input() min = 0 // 最小值
+
+  @Input() max = 10000 // 最大值
+
   add = () => {
-    this.value++
+    this.value < this.max && this.value++
     debounce(() => this.valueChange.emit(this.value), 1000)
   }
 
   setValue = (e: Event) => {
     let target = e.target as HTMLInputElement
     debounce(() => {
-      this.value = Number(target.value.replace(/[^\d]/g, ''))
+      this.value = Math.min(Math.max(Number(target.value.replace(/[^\d]/g, '')), this.min), this.max)
       target.value = this.value.toString()
       this.valueChange.emit(this.value)
     }, 1000)
   }
 
   sub = () => {
-    this.value > 0 && this.value--
+    this.value > this.min && this.value--
     debounce(() => this.valueChange.emit(this.value), 1000)
   }
 }
