17552936b713b157cf63333330c49b65efa9d84b
sadanya <2930048068@qq.com>
2021-11-03 14:07:02 +0800

    更了

--- angular/yz/src/component/readonly-table/readonly-table.component.ts
+++ angular/yz/src/component/readonly-table/readonly-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from "@angular/core";
 
 import { RTBody } from "../../common/model";
 
@@ -9,7 +9,7 @@ import { body, head } from "./readonly-table.test";
   templateUrl: './readonly-table.component.html',
   styleUrls: ['./readonly-table.component.css']
 })
-export class ReadonlyTableComponent implements OnChanges {
+export class ReadonlyTableComponent implements OnChanges, OnInit {
 
   constructor() { }
 
@@ -27,50 +27,36 @@ export class ReadonlyTableComponent implements OnChanges {
 
   // body 是源, 首先根据搜索内容截取成 sbody, 在依此来排序成 obody, 最后分页成 pbody 应用
 
-  pbody: RTBody[] = [] // 分页
-
   sbody: RTBody[] = [] // 搜索
 
   obody: RTBody[] = [] // 排序
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.body && !changes.body.isFirstChange()) this.bodySearch(this.body)
-  }
+  pbody: RTBody[] = [] // 分页
 
-  // 根据页数来分割表格体
-  bodySlice = <T>(body: T[], line: number, page: number) => {
+  ngOnInit() { this.tsearch(this.body) }
 
-    let arr: T[] = []
-    let start = line * (page - 1)
-    let end = Math.min(line * page, body.length)
+  ngOnChanges(changes: SimpleChanges) { if (changes.body && !changes.body.isFirstChange()) this.tsearch(this.body) }
 
-    for (let i = start; i < end; i++)
-      arr.push(body[i])
-
-    return arr
-
-  }
-
-  // 翻页
-  pageChange = (e: number) => this.pbody = this.bodySlice(this.obody, this.line, e)
-
-  // 搜索特定内容
-  bodySearch = (e: RTBody[]) => {
+  // 搜索
+  tsearch = (e: RTBody[]) => {
 
     this.sbody = e
     this.max = Math.ceil(this.sbody.length / this.line) // 最大页数由搜索后的内容决定
 
-    this.bodySort(e)
+    this.tsort(e)
 
   }
 
   // 排序
-  bodySort = (e: RTBody[]) => {
+  tsort = (e: RTBody[]) => {
 
     this.obody = e
 
-    this.pageChange(this.page)
+    this.tpage(this.page)
 
   }
+
+  // 翻页
+  tpage = (e: number) => this.pbody = this.obody.slice(this.line * (e - 1), Math.min(this.line * e, body.length))
 
 }
--- angular/yz/src/component/readonly-table/readonly-table.test.ts
+++ angular/yz/src/component/readonly-table/readonly-table.test.ts
@@ -8,5 +8,14 @@ export const head: RTHead[] = [
 
 export const body: RTBody[] = [
   { id: 1, name: 'chengzs', type: 'human' },
-  { id: 2, name: 'sadanya', type: 'human' }
+  { id: 2, name: 'sadanya', type: 'human' },
+  { id: 3, name: 'satania', type: 'animation' },
+  { id: 4, name: 'zerzz', type: 'human' },
+  { id: 5, name: 'sadanikya', type: 'human' },
+  { id: 6, name: 'abc', type: 'number' },
+  { id: 7, name: 'eu4', type: 'game' },
+  { id: 8, name: 'dont starve', type: 'game' },
+  { id: 9, name: 'dst', type: 'game' },
+  { id: 10, name: 'lol', type: 'game' },
+  { id: 11, name: 'doki doki', type: 'game' },
 ]
