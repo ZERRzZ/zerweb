c49c92bc48f09eb09c8f44825a70f61fd4f33145
chengzs <2930048068@qq.com>
2022-02-28 17:34:23 +0800

    维护 @1.7.1

--- angular/yz/common/method/date.ts
+++ angular/yz/common/method/date.ts
@@ -1,12 +1,12 @@
 /**
- * 转日期字符串如 2021-07-30
- */
-export const Y_M_D = (date: Date) => `${date.getFullYear()}-${MM(date.getMonth() + 1)}-${MM(date.getDate())}`
-
-/**
  * 转日期字符串如 08-20
  */
 export const M_D = (date: Date) => `${MM(date.getMonth() + 1)}-${MM(date.getDate())}`
+
+/**
+ * 转日期字符串如 2021-07-30
+ */
+export const Y_M_D = (date: Date) => `${date.getFullYear()}-${MM(date.getMonth() + 1)}-${MM(date.getDate())}`
 
 /**
  * 转日期字符串如 20210730
@@ -14,7 +14,7 @@ export const M_D = (date: Date) => `${MM(date.getMonth() + 1)}-${MM(date.getDate
 export const YYYYMMDD = (date: Date) => `${date.getFullYear()}${MM(date.getMonth() + 1)}${MM(date.getDate())}`
 
 /**
- * 转日期字符串如 2021 07 30 17
+ * 转日期字符串如 2021073017
  */
 export const YYYYMMDDHH = (date: Date) => `${date.getFullYear()}${MM(date.getMonth() + 1)}${MM(date.getDate())}${MM(date.getHours())}`
 
@@ -32,3 +32,9 @@ export const dailyYMDH = (date: Date) => `${date.getFullYear()}年${MM(date.getM
  * 格式化 M to MM
  */
 export const MM = (number: number) => number > 9 ? number : `0${number}`
+
+/**未来或过去几天
+ * @param date 源日期
+ * @param days 未来(正值)或过去(负值)天数
+ */
+export const jumpDays = (date: Date, days: number) => new Date(date.getFullYear(), date.getMonth(), date.getDate() + days)
