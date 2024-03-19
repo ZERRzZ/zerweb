2be943801d574af8dfd4d94c8b500325fcd2aa06
Sadanya <2930048068@qq.com>
2024-03-19 17:29:06 +0800

    装包，格式优化

--- react/cms-admin-v3/src/stories/ConfigStore.tsx
+++ react/cms-admin-v3/src/stories/ConfigStore.tsx
@@ -1,4 +1,5 @@
-import React, {Dispatch, useReducer, useEffect} from "react";
+import React, { Dispatch, useReducer, useEffect } from "react";
+
 interface IConfigContext {
   menuCollapsed: boolean;
   userInfo: any;
@@ -23,59 +24,60 @@ const reducer = (prevState: IConfigContext, action: ActionType) => {
         ...prevState,
         menuCollapsed: action.value
       };
-      case "userInfo":
-        return {
-          ...prevState,
-          userInfo: action.value
-        };
-      case "menuParentKey":
-        return {
-          ...prevState,
-          menuParentKey: action.value
-        };
+    case "userInfo":
+      return {
+        ...prevState,
+        userInfo: action.value
+      };
+    case "menuParentKey":
+      return {
+        ...prevState,
+        menuParentKey: action.value
+      };
     default:
       return prevState
   }
 };
 
-export const ConfigContext = React.createContext({} as { 
+export const ConfigContext = React.createContext({} as {
   state: IConfigContext;
   dispatch: Dispatch<ActionType>;
-  userHasPems: (pems?: string[]) => boolean; });
+  userHasPems: (pems?: string[]) => boolean;
+});
 
 export const ConfigProvider: React.FC = props => {
   let [state, dispatch] = useReducer(reducer, defaultState);
 
-  useEffect(()=>{
-    
-  },[])
+  useEffect(() => {
+
+  }, [])
 
   const userHasAuth = (auth: string) => {
     if (state.userInfo && state.userInfo.authList) {
-        return state.userInfo.authList.includes(auth);
+      return state.userInfo.authList.includes(auth);
     }
     return false;
-};
+  };
 
-const userHasRole = (role: string) => {
+  const userHasRole = (role: string) => {
     if (state.userInfo && state.userInfo.roleList) {
-        return state.userInfo.roleList.includes(role);
+      return state.userInfo.roleList.includes(role);
     }
     return false;
-};
+  };
 
-const userHasPems = (pems?: string[]) => {
-    if(pems && pems.length) {
-        for(let i = 0; i < pems.length; i++) {
-            let array = pems[i].split(',');
-            if (array.every(a => userHasAuth(a))) {
-                return true;
-            }
+  const userHasPems = (pems?: string[]) => {
+    if (pems && pems.length) {
+      for (let i = 0; i < pems.length; i++) {
+        let array = pems[i].split(',');
+        if (array.every(a => userHasAuth(a))) {
+          return true;
         }
-        return false;
+      }
+      return false;
     }
     return true;
-}
+  }
 
-  return <ConfigContext.Provider {...props} value={{state, dispatch, userHasPems}}/>
+  return <ConfigContext.Provider {...props} value={{ state, dispatch, userHasPems }} />
 };
