9805bd13ca0bbba4f7531cfb3fb8c1b8c565c1be
chengzs <2930048068@qq.com>
2024-07-24 16:50:49 +0800

    添加 vue 组件 iconfont

---
+++ vue/component/IconFont/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg class="iconfont" aria-hidden="true" :font-size="size" :color="color">
+    <use :xlink:href="`#${type}`"></use>
+  </svg>
+</template>
+
+<script lang="ts" setup>
+  defineProps({
+    type: {
+      type: String,
+    },
+    size: {
+      type: [String, Number],
+      default: '1em',
+    },
+    color: {
+      type: String,
+      default: 'inherit',
+    },
+  });
+</script>
+
+<style scoped>
+  .iconfont {
+    width: 1em;
+    height: 1em;
+    margin: 0 5px;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+  }
+</style>
